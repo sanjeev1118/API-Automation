32ca4aeb6ec8f102704c8b26659d616d3bec02db
Sanjeev Kumar <kumarsanjeev1118@gmail.com>
2023-01-07 18:28:13 +0530

    setup rest assured with a test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,33 @@
 package org.example;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Main {
     public static void main(String[] args) {
+        RestAssured.baseURI = "https://demoqa.com/BookStore/v1/Books";
+        RequestSpecification httprequest = RestAssured.given();
+        Response response = httprequest.header("content-type","application/json").when().get();
+        System.out.println("Output :- " + response.header("Connection"));
+        response.then().log().all().statusCode(200).statusLine("HTTP/1.1 200 OK");
+        JsonPath jsonPath = response.body().jsonPath();
+        List<Object> list = jsonPath.getList("books");
 
-        System.out.println("Hello world!");
+        for(Object o: list) {
+            LinkedHashMap<String,Object> map = (LinkedHashMap<String, Object>) o;
+            System.out.println("Result:- "+ map.get("title"));
+            if(map.get("title").equals("Speaking JavaScript"))
+                System.out.println("Author Name:- "+ map.get("author"));
+        }
+
     }
 }
